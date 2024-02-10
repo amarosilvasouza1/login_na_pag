da8385d7ac21764d9415196c86cbeb788b89144a
amaro silva <amarosilvasouza1@gmail.com>
2024-02-10 16:19:46 -0300

    fazendo a logoca de pega os inpults dee senha e email,  e maracndo oque vc tem quee fazer no codigo boa sorte 0w0

---
+++ src/script/data.mjs
@@ -0,0 +1,47 @@
+
+const users = [
+    {
+        id: 1,
+        name: "amaro123",
+        password: "123456",
+        email: "amarosilva@gmail.com",
+        acesso: true,
+    },
+    {
+        id: 1,
+        name: "ugo",
+        password: "123456",
+        email: "ugo@gmail.com",
+        acesso: false,
+    },
+    {
+        id: 1,
+        name: "senpai",
+        password: "132435",
+        email: "senpai@gmail.com",
+        acesso: false,
+    },
+    {
+        id: 1,
+        name: "felipe",
+        password: "098765",
+        email: "felipe@gmail.com",
+        acesso: false,
+    },
+    {
+        id: 1,
+        name: "md_chef",
+        password: "123456",
+        email: "md_chef@gmail.com",
+        acesso: false,
+    },
+    {
+        id: 1,
+        name: "neymar",
+        password: "123456",
+        email: "ney2@gmail.com",
+        acesso: false,
+    }
+]
+
+export default users
